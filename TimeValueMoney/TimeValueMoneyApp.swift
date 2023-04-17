368f8c804879b19b58d61c3f938400340ab3016c
Nicholas Pompea <nick.pompea@gmail,com>
2023-04-17 11:27:24 -0600

    Initial Commit

---
+++ TimeValueMoney/TimeValueMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeValueMoneyApp.swift
+//  TimeValueMoney
+//
+//  Created by Nicholas Pompea on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeValueMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
